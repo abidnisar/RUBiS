b9feef19327aa3239456802e0ac43c1017d27696
abidnisar <abid.nisar2@yahoo.com>
2015-03-16 00:43:43 +0500

    update rubis cb 2

--- Rubis_CB/recipes/default.rb
+++ Rubis_CB/recipes/default.rb
@@ -4,5 +4,5 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 execute "copy-rubis" do
-  command "sudo cp -a ~/RUBiS/PHP /var/www"
+  command "sudo cp -a /home/ubuntu/RUBiS/PHP /var/www"
 end

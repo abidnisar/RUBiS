af483be8969a23f28b1dd0e4b0fc3ed9ff86d4a0
abidnisar <abid.nisar2@yahoo.com>
2015-03-15 23:41:53 +0500

    update rubis-cb

--- Rubis_CB/recipes/default.rb
+++ Rubis_CB/recipes/default.rb
@@ -4,6 +4,6 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 execute "copy-rubis" do
-  command "cp -a ~/RRBiS/PHP /var/www"
-  command "find /var/www/PHP -type f -print0 | xargs -0 sed -i ‘s/HTTP_GET_VARS/_GET/g’
+  command "sudo cp -a ~/RUBiS/PHP /var/www"
+  command "sudo find /var/www/PHP -type f -print0 | xargs -0 sed -i 's/HTTP_GET_VARS/_GET/g'"
 end

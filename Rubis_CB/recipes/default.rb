54f19d6817baaa974f316da22da8fa2c12e0ce65
abidnisar <abid_nisar2@yahoo.com>
2015-03-17 00:06:38 +0500

    Update default.rb

--- Rubis_CB/recipes/default.rb
+++ Rubis_CB/recipes/default.rb
@@ -5,5 +5,6 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 execute "copy-rubis" do
   command "sudo mkdir /var/www/html"
-  command "sudo cp -a /home/ubuntu/RUBiS/PHP /var/www/html/"
+  command "sudo mkdir /var/www/html/PHP"
+  command "sudo cp -a /home/ubuntu/RUBiS/PHP /var/www/html/PHP"
 end
